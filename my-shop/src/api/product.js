135f5e78ca8fd6dd75f714ebe94309e7e318ac28
南极之光 <1780535983@qq.com>
2022-05-13 20:25:44 +0800

    添加评论个数和评论列表信息接口

--- my-shop/src/api/product.js
+++ my-shop/src/api/product.js
@@ -14,3 +14,18 @@ export const getProductDeatail = productId => {
     url: `/product/detail/${productId}`
   })
 }
+// 获取评价个数/reply/config/:productId
+export const getCommentCount = productId => {
+  return request({
+    method: 'get',
+    url:`/reply/config/${productId}`
+  })
+}
+// 根据分类获取评价：/reply/list/:productId
+export const getCommentByTag = (productId, params) => {
+  return request({
+    method: 'get',
+    url: `/reply/list/${productId}`,
+    params
+  })
+}
